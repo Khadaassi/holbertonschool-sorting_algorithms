630e854ff432ddb962b4c1fcf385f6be4f56c6c8
Khadaassi <aassikhadija@gmail.com>
2024-01-16 12:58:30 +0100

    Fix formatting in bubble_sort function

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -3,7 +3,7 @@
  * bubble_sort - sorts an array of integers in ascending order
  * @array: array of integers
  * @size: size of array
-*/
+ */
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
