f3e9be480163cb634658f312259095bbd5bc6daf
Khadaassi <aassikhadija@gmail.com>
2024-01-16 10:01:34 +0100

    Add bubble sort algorithm implementation
    This commit adds the implementation of the bubble sort algorithm in the file "0-bubble_sort.c". The algorithm sorts an array of integers in ascending order.

---
+++ 0-bubble_sort.c
@@ -0,0 +1,28 @@
+#include "sort.h"
+/**
+ * bubble_sort - sorts an array of integers in ascending order
+ * @array: array of integers
+ * @size: size of array
+*/
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+	int tmp;
+
+	if (array == NULL || size < 2)
+		return;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 1; j < size - i; j++)
+		{
+			if (array[j - 1] > array[j])
+			{
+				tmp = array[j - 1];
+				array[j - 1] = array[j];
+				array[j] = tmp;
+				print_array(array, size);
+			}
+		}
+	}
+}
